311644c47cd81376b260ce888b997b0b399ec098
Ricardo Nevarez Jr <125226505+rnevarez-cloud@users.noreply.github.com>
2024-01-30 09:29:13 -0500

    Update mass-deploy-patch.ps1

--- mass-deploy-patch.ps1
+++ mass-deploy-patch.ps1
@@ -1,15 +1,41 @@
-### Mass Deploy Patch
-### Author: Ricardo Nevarez
+### Install Application Patch ###
+### Author: Ricardo Nevarez ###
+### Revision: 20240126 ###
 
-clear
+Clear-Host
 
-$patch = Read-Host "Enter the path of the application patch" ## Example: "C:\Downloads\Patch"
-$exclude = Read-Host "Enter the clients that must be excluded (ex. app_1,app_2,app_3)" ## List of folders to exclude. Example: "app_1,app_2,app_3"
-$exclude = $exclude.split(",")
-
-$folders = Get-ChildItem c:\webroot\ -Exclude $exclude -Directory
-
+$patch = Read-Host "Enter the path of the Application patch" ## Example: "C:\ApplicationDownloads\Application_Patch\Application_Patch"
 $patch = "$($patch)\*"
+$folders = Get-ChildItem D:\Application\ -Directory
+Write-Host "--------------------------------------------------------------------------"
+Write-Host $folders
+
+do {
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "Include or exclude?"
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "1. Include" -ForegroundColor Yellow 
+    Write-Host "2. Exclude" -ForegroundColor Yellow
+    Write-Host "--------------------------------------------------------------------------"
+
+    $choice = Read-Host "Enter choice"
+
+} until (($choice -match '[1-2]'))
+
+switch($choice) {
+    '1' {
+        Write-Host "--------------------------------------------------------------------------"
+        $include = Read-Host "Enter the clients that must be included (ex. ABC_Application,XYZ_Application)" ## List of folders to include. Example: "ABC_Application,XYZ_Application,DEF_Application"
+        $include = $include.split(",")   
+        $folders = Get-ChildItem D:\Application\ -Exclude $exclude -Directory
+    }
+    '2' {
+        Write-Host "--------------------------------------------------------------------------"
+        $exclude = Read-Host "Enter the clients that must be excluded (ex. ABC_Application,XYZ_Application)" ## List of folders to exclude. Example: "ABC_Application,XYZ_Application,DEF_Application"
+        $exclude = $exclude.split(",")
+        $folders = Get-ChildItem D:\Application\ -Exclude $exclude -Directory
+    }
+}
 
 Write-Host $patch
 
