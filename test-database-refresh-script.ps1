c83a5bc885d070d65f484594782c3a1b3dcbba4f
Ricardo Nevarez Jr <125226505+rnevarez-cloud@users.noreply.github.com>
2024-01-30 09:31:12 -0500

    Update test-database-refresh-script.ps1

--- test-database-refresh-script.ps1
+++ test-database-refresh-script.ps1
@@ -1,4 +1,6 @@
-###Test Database Refresh Script
+##### Ricardo Nevarez - Application Test Database Refresh Script #####
+##### Revision: 20240129 #####
+
 Clear-Host
 
 ##### Setting variables #####
@@ -58,15 +60,21 @@ if($verify -eq "N") {
 
 $timestamp = get-date -format yyyy-MM-dd
 
-$productionDatabase = "$($client)_production"
+$productionDatabase = "$($client)_Application"
 $testDatabase = "$($client)_test"
 $tempDatabase = "$($testDatabase)_temp"
+
+##### Grabbing version info for test before a restore from production #####
+$testVersionInfo = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT VERSION, BUILD FROM SYSVAR"
 
 if(Test-Path $productionServer) {
     $backupFile = 1
     $productionFilePath = $productionServer
 } else {
-    $productionFilePath = "$($tempDirectory)\$($productionDatabase)_backup_$($timestamp).bak"
+    $prodBackupDrive = (Get-Volume -FileSystemLabel *Backup*).DriveLetter
+    $prodBackupDriveUNC = "$($prodBackupDrive)$"
+    $prodTempDirectory = "$($prodBackupDriveUNC)\Temp"
+    $productionFilePath = "\\$($productionServer)\$($prodTempDirectory)\$($productionDatabase)_backup_$($timestamp).bak"
 }
 
 $testFilePath = "$($tempDirectory)\$($testDatabase)_backup_$($timestamp).bak"
@@ -74,16 +82,35 @@ $testFilePath = "$($tempDirectory)\$($testDatabase)_backup_$($timestamp).bak"
 ##### Check for temp directory #####
 
 if(Test-Path $tempDirectory) {
-    Remove-Item "$($tempDirectory)\*" -Recurse -Force
+    do {
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "Files in $($tempDirectory)" -ForegroundColor Cyan
+    Get-ChildItem $tempDirectory
+    Write-Host "--------------------------------------------------------------------------"
+
+    $verify = Read-Host "Do you wish to clear the temp directory (Y/N)?"
+
+    } until (($verify -eq "Y") -or ($verify -eq "N"))
+
+    if($verify -eq "Y") {
+        Write-Host "Clearing $($tempDirectory)" -ForegroundColor Yellow
+        Remove-Item "$($tempDirectory)\*" -Recurse -Force
+    } else {
+        Write-Host "$($tempDirectory) will not be cleared" -ForegroundColor Yellow
+    }
+    
 } else {
-    New-Item "tempDirectory" -ItemType "Directory"
+    New-Item $tempDirectory -ItemType "Directory"
 }
 
 if($backupFile -eq 0) {
-    if(Test-Path "\\$($productionServer)\E$\Temp\") {
-        Remove-Item "\\$($productionServer)\E$\Temp\*" -Recurse -Force
+
+    $prodTempUNCPath = "\\$($productionServer)\$($prodBackupDriveUNC)\Temp"
+
+    if(Test-Path $prodTempUNCPath) {
+        Remove-Item "$($prodTempUNCPath)\*" -Recurse -Force
     } else {
-        New-Item "\\$($productionServer)\E$\Temp" -ItemType "Directory"
+        New-Item $prodTempUNCPath -ItemType "Directory"
     }    
 
     ##### Taking SQL backups #####
@@ -99,10 +126,15 @@ if($backupFile -eq 0) {
         Write-Host "****************************************" -ForegroundColor Red
         Write-Host "An error occurred:" -ForegroundColor Red
         Write-Host $_ -ForegroundColor Red
-        Read-Host "Please copy a SQL backup of the production database to the test server, then re-run the script." 
         Write-Host "****************************************" -ForegroundColor Red
+        Read-Host "Please copy a SQL backup of the production database to the test server, then re-run the script." 
+
+        exit;
+
     }
-    Copy-Item -Path "\\$($productionServer)\E$\Temp\*" -Destination "$($tempDirectory)"
+
+    Copy-Item -Path "\\$($productionServer)\$($prodBackupDriveUNC)\Temp\*" -Destination "$($tempDirectory)"
+    $productionFilePath = "$($tempDirectory)\$($productionDatabase)_backup_$($timestamp).bak"
 
 }
 
@@ -131,8 +163,8 @@ Write-Host "--------------------------------------------------------------------
 Write-Host "Creating temporary database $($tempDatabase)" -ForegroundColor Cyan
 Write-Host "--------------------------------------------------------------------------"
 
-$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("db_Data", "$($tempDirectory)\$($testDatabase).mdf")
-$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("db_Log", "$($tempDirectory)\$($testDatabase).ldf")
+$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("ApplicationWeb100db_Data", "$($tempDirectory)\$($testDatabase).mdf")
+$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("ApplicationWeb100db_Log", "$($tempDirectory)\$($testDatabase).ldf")
 
 try {
     Restore-SqlDatabase -ServerInstance $testServer -Database $tempDatabase -BackupFile $testFilePath -RelocateFile @($RelocateData,$RelocateLog) -ErrorAction Stop
@@ -147,6 +179,7 @@ catch {
 
 Invoke-Sqlcmd -ServerInstance $testServer -QueryTimeout 0 -Query "ALTER DATABASE $($testDatabase) SET ONLINE"
 
+
 ##### Restoring test configuration values to test database #####
 
 Write-Host "--------------------------------------------------------------------------"
@@ -158,15 +191,17 @@ $table2 = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Que
 $table3 = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT table3 = STUFF((SELECT ',' + '[' + COLUMN_NAME + ']' FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'table3' FOR XML PATH('')), 1, 1, '')"
 $table4 = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT table4 = STUFF((SELECT ',' + '[' + COLUMN_NAME + ']' FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'table4' FOR XML PATH('')), 1, 1, '')"
 $table5 = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT table5 = STUFF((SELECT ',' + '[' + COLUMN_NAME + ']' FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'table5' FOR XML PATH('')), 1, 1, '')"
+$table6 = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT table6 = STUFF((SELECT ',' + '[' + COLUMN_NAME + ']' FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'table6' FOR XML PATH('')), 1, 1, '')"
 
 $query = @(
-    "TRUNCATE TABLE IZ_CONFIG; TRUNCATE TABLE SECURITY_SSO; TRUNCATE TABLE NMSSO; TRUNCATE TABLE SAMLIDPS"
-    "SET IDENTITY_INSERT IZ_CONFIG ON; INSERT INTO IZ_CONFIG ($($table1.Item(0))) SELECT * FROM $($tempDatabase).dbo.IZ_CONFIG; SET IDENTITY_INSERT IZ_CONFIG OFF"
-    "SET IDENTITY_INSERT SECURITY_SSO ON; INSERT INTO SECURITY_SSO ($($SECURITY_SSO.Item(0))) SELECT * FROM $($tempDatabase).dbo.SECURITY_SSO; SET IDENTITY_INSERT SECURITY_SSO OFF"
-    "SET IDENTITY_INSERT NMSSO ON; INSERT INTO NMSSO ($($NMSSO.Item(0))) SELECT * FROM $($tempDatabase).dbo.NMSSO; SET IDENTITY_INSERT NMSSO OFF"
-    "SET IDENTITY_INSERT SAMLIDPS ON; INSERT INTO SAMLIDPS ($($SAMLIDPS.Item(0))) SELECT * FROM $($tempDatabase).dbo.SAMLIDPS; SET IDENTITY_INSERT SAMLIDPS ON"
-    "SET IDENTITY_INSERT NELNET ON; INSERT INTO NELNET ($($NELNET.Item(0))) SELECT * FROM $($tempDatabase).dbo.NELNET; SET IDENTITY_INSERT NELNET OFF"
-    "UPDATE API_CONF SET PORT = 'XXXXX' WHERE API_COD = 'INTEGRATION'"
+    "TRUNCATE TABLE table1; TRUNCATE TABLE table2; TRUNCATE TABLE table3; TRUNCATE TABLE table4"
+    "SET IDENTITY_INSERT table1 ON; INSERT INTO table1 ($($table1.Item(0))) SELECT * FROM $($tempDatabase).dbo.table1; SET IDENTITY_INSERT table1 OFF"
+    "SET IDENTITY_INSERT table2 ON; INSERT INTO table2 ($($table2.Item(0))) SELECT * FROM $($tempDatabase).dbo.table2; SET IDENTITY_INSERT table2 OFF"
+    "SET IDENTITY_INSERT table3 ON; INSERT INTO table3 ($($table3.Item(0))) SELECT * FROM $($tempDatabase).dbo.table3; SET IDENTITY_INSERT table3 OFF"
+    "SET IDENTITY_INSERT table4 ON; INSERT INTO table4 ($($table4.Item(0))) SELECT * FROM $($tempDatabase).dbo.table4; SET IDENTITY_INSERT table4 ON"
+    "INSERT INTO table5 ($($table5.Item(0))) SELECT * FROM $($tempDatabase).dbo.table5"
+    "INSERT INTO table6 ($($table6.Item(0))) SELECT * FROM $($tempDatabase).dbo.table6"
+    "UPDATE API_CONF SET PORT = '21580' WHERE API_COD = 'XXX'"
     )   
 
 foreach($q in $query) {
@@ -182,47 +217,61 @@ foreach($q in $query) {
     }
 }
 
-Invoke-Sqlcmd -ServerInstance $testServer -Query "DROP DATABASE $($tempDatabase)"
+##### Grabbing version info for test after a restore from production #####
+$prodVersionInfo = Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "SELECT VERSION, BUILD FROM SYSVAR"
 
-$detectJRM = Invoke-Sqlcmd -ServerInstance $testServer -QueryTimeout 0 -Query "SELECT NAME FROM master.sys.server_principals WHERE NAME = '$($client)_integration'"
+Invoke-Sqlcmd -ServerInstance $testServer -QueryTimeout 0 -Query "DROP DATABASE $($tempDatabase)" 
+
+$detectAPP = Invoke-Sqlcmd -ServerInstance $testServer -QueryTimeout 0 -Query "SELECT NAME FROM master.sys.server_principals WHERE NAME = '$($client)_APPi'"
 
 Write-Host "--------------------------------------------------------------------------"
-Write-Host "Checking for Integration..." -ForegroundColor Cyan
+Write-Host "Checking for APP..." -ForegroundColor Cyan
 Write-Host "--------------------------------------------------------------------------"
 
-if($null -ne $detectJRM) {
+if($null -ne $detectAPP) {
     Write-Host "--------------------------------------------------------------------------"
-    Write-Host "Integration found! Preserving Integration login." -ForegroundColor Green
+    Write-Host "APP integration found! Preserving APP login." -ForegroundColor Green
     Write-Host "--------------------------------------------------------------------------"
     
-    Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "EXEC sp_change_users_login  Auto_Fix, $($client)_integration; GRANT CONTROL ON CERTIFICATE::SSN TO $($client)_integration; GRANT CONTROL ON SYMMETRIC KEY::KEY TO $($client)_integration"
+    Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "EXEC sp_change_users_login  Auto_Fix, $($client)_APPi; GRANT CONTROL ON CERTIFICATE::SSN TO $($client)_APPi; GRANT CONTROL ON SYMMETRIC KEY::SSN_Key_01 TO $($client)_APPi"
 } else {
     Write-Host "--------------------------------------------------------------------------"
-    Write-Host "Integration not found. Skipping." -ForegroundColor Yellow
+    Write-Host "APP integration not found. Skipping." -ForegroundColor Yellow
     Write-Host "--------------------------------------------------------------------------"
 }
+
+Invoke-Sqlcmd -ServerInstance $testServer -Database $testDatabase -Query "OPEN MASTER KEY DECRYPTION BY PASSWORD = 'Application123RJM123Systems123'; ALTER MASTER KEY ADD ENCRYPTION BY SERVICE MASTER KEY"
 
 Write-Host "--------------------------------------------------------------------------"
 Write-Host "$($testDatabase) refresh complete!" -ForegroundColor Green
 Write-Host "--------------------------------------------------------------------------"
 
-$testAppServer = Read-Host "Enter the name of the client's TEST CONTOSO WEB server"
-$testInstanceNumber = Read-Host "Enter the client's TEST CONTOSO instance number"
+##### Comparing versions between production and test databases #####
+if(($testVersionInfo.Version -ne $prodVersionInfo.Version) -and ($testVersionInfo.Build -ne $prodVersionInfo.Build)) {
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "Version mismatch detected between production and test. Please make sure to run scripts on the test environment." -ForegroundColor Yellow
+    Write-Host "Test Version Information: $($testVersionInfo.Version) ($($testVersionInfo.Build))" -ForegroundColor Cyan
+    Write-Host "Production Version Information: $($prodVersionInfo.Version) ($($prodVersionInfo.Build))" -ForegroundColor Cyan
+    Write-Host "--------------------------------------------------------------------------"
+}
+
+$testAppServer = Read-Host "Enter the name of the client's TEST Application WEB server"
+$testInstanceNumber = Read-Host "Enter the client's TEST Application instance number"
 
 $testConnection = Test-Connection $testAppServer -Quiet -Count 1
 
 if(!($testConnection)) {
     Write-Host "--------------------------------------------------------------------------"
-    Write-Host "CONTOSO test server inaccessible from this server. Please clear the caches manually." -ForegroundColor Red
+    Write-Host "Application test server inaccessible from this server. Please clear the ColdFusion caches manually." -ForegroundColor Red
     Write-Host "--------------------------------------------------------------------------"
 } else {
     Write-Host "--------------------------------------------------------------------------"
-    Write-Host "Restarting $($client)'s CONTOSO TEST instance" -ForegroundColor Cyan
+    Write-Host "Restarting $($client)'s Application TEST instance" -ForegroundColor Cyan
     Write-Host "--------------------------------------------------------------------------"
 
     Invoke-Command -ComputerName $testAppServer -ScriptBlock {
-        $testSonisInstance = Get-Service "Application Server contoso_$($testInstanceNumber)"
-        $testSonisInstance | %{ Restart-Service $_ }
+        $testApplicationInstance = Get-Service "ColdFusion **** Application Server Application_$($testInstanceNumber)"
+        $testApplicationInstance | %{ Restart-Service $_ }
     }
 
     Write-Host "--------------------------------------------------------------------------"
